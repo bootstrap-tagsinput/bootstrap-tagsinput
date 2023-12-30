47566d5228ad1de20c7ba4b1a22f2d9ba3fd7c22
Oren Forer <oforer@gmail.com>
2023-12-30 09:32:27 -0500

    Fixes Cross-Site Scripting in bootstrap-tagsinput
    All versions of bootstrap-tagsinput are vulnerable to cross-site scripting when user input is passed into the itemTitle parameter unmodified,
    as the package fails to properly sanitize or encode user input for that parameter.

--- src/bootstrap-tagsinput.js
+++ src/bootstrap-tagsinput.js
@@ -136,7 +136,7 @@
 
       // add a tag element
 
-      var $tag = $('<span class="tag ' + htmlEncode(tagClass) + (itemTitle !== null ? ('" title="' + itemTitle) : '') + '">' + htmlEncode(itemText) + '<span data-role="remove"></span></span>');
+      var $tag = $('<span class="tag ' + htmlEncode(tagClass) + (itemTitle !== null ? ('" title="' + htmlEncode(itemTitle)) : '') + '">' + htmlEncode(itemText) + '<span data-role="remove"></span></span>');
       $tag.data('item', item);
       self.findInputWrapper().before($tag);
       $tag.after(' ');
